8759bafe547fdf86733c026be3643f2981d47d14
Kayli Garwood <kgarwood@uncc.edu>
2020-10-13 17:08:17 -0400

    Brought Axios and ChartJS into the application and got Chart working

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,52 @@
 import React from 'react';
+import axios from 'axios';
+import Chart from 'chart.js';
+
+var dataSource = {
+    datasets: [
+        {
+            data: [],
+            backgroundColor: [
+                '#ff3333',
+                '#ff9f33',
+                '#fff833',
+                '#87ff33',
+                '#33fdff',
+                '#3354ff',
+                '#ac33ff',
+                '#fc33ff',
+                '#ff3386',
+                '#34ffad',
+                '#33bdff',
+            ],
+        },
+    ],
+    labels: []
+};
+
+function createChart() {
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: dataSource
+    });
+}
+
+function getBudget() {
+    axios.get('http://localhost:5000/budget')
+    .then(function (res) {
+        console.log(res.data);
+        for (var i = 0; i < res.data.myBudget.length; i++){
+            dataSource.datasets[0].data[i] = res.data.myBudget[i].budget;
+            dataSource.labels[i] = res.data.myBudget[i].title;
+        }
+        createChart();
+    });
+}
+getBudget();
+
+
+
 
 function HomePage() {
   return (
